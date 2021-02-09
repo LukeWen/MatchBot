88f05d87bdb21041879648b2486eb3676ca6b34f
LukeWen <LukeWen@users.noreply.github.com>
2021-02-09 23:32:42 +0800

    完善功能

--- matchBot.py
+++ matchBot.py
@@ -1,23 +1,95 @@
 # -*- coding: utf-8 -*-
 import json
+from datetime import datetime, timedelta
 from khl import TextMsg, Bot, Cert
 
 # load config from config/botConfig.json, replace `path` points to your own config file
 # config template: `./config/botConfig.json.example`
 with open('./config/botConfig.json', 'r', encoding='utf-8') as f:
     config = json.load(f)
-print(config)
+
+with open('./jsonTemplate/menu.json', 'r', encoding='utf-8') as menu:
+    menuJson = json.load(menu)
+
 # init Cert and Bot
 cert = Cert(client_id = config['client_id'], client_secret = config['client_secret'], token = config['token'])
 bot = Bot(cmd_prefix=['.', '。'], cert=cert)
+matchBoard = dict()
 
-
-@bot.command(name='hello')
+@bot.command(name='菜单')
 async def roll(msg: TextMsg):
-    await msg.reply('world!')
+    await msg.ctx.send_card_temp(menuJson)
 
+@bot.command(name='组队')
+async def roll(msg: TextMsg, t_info: str, t_time: str = 300):
+    int_time = 0
+    try:
+        int_time = int(t_time)
+    except ValueError as e:
+        await msg.reply_temp('时间必须是数字')
+    else:
+        if int_time > 1800:
+            int_time = 1800
+        if int_time < 10:
+            await msg.reply_temp('时间过短请重新申请')
+        else:
+            ddl = datetime.now() + timedelta(seconds = int(t_time))
+            matchInfo = (t_info, ddl)
+            matchBoard[msg.author_id] = matchInfo
+            await msg.reply_temp('组队成功:(met)' + msg.author_id + '(met) 截止时间：' + ddl.strftime("%Y-%m-%d %H:%M:%S") )
+
+
+@bot.command(name='取消组队')
+async def roll(msg: TextMsg):
+    matchBoard.pop(msg.author_id)
+    await msg.reply_temp('取消组队成功')
+
+@bot.command(name='所有组队')
+async def roll(msg: TextMsg):
+    nowtime = datetime.now()
+    global matchBoard
+    matchBoard = filter_dict(matchBoard, lambda k,v: nowtime < v[1])
+    if (len (matchBoard) < 1):
+        await msg.reply_temp('还没有人组队哦')
+    else:
+        replyJson = '''[
+            {
+            "type": "card",
+            "theme": "secondary",
+            "size": "lg",
+            "modules": [
+                {
+                "type": "section",
+                "text": {
+                    "type": "kmarkdown",
+                    "content": "**组队信息**"
+                }
+            },
+            {
+            "type": "section",
+            "text": {
+                "type": "paragraph",
+                "rows": ''' + str(len (matchBoard)) + ''',
+                "fields": ['''
+        for key, values in matchBoard.items():
+            replyJson = replyJson + '''{
+                "type": "kmarkdown",
+                "content": "发布者：(met)'''+ str(key) +'''(met)\\n信息：''' + values[0] + '''\\t截止时间：''' + values[1].strftime("%m-%d %H:%M:%S") + '''"
+              },'''
+        replyJson = replyJson[:-1]
+        replyJson = replyJson + ']}}]}]'
+        await msg.ctx.send_card_temp(replyJson)
+
+def filter_dict(d, f):
+    ''' Filters dictionary d by function f. '''
+    newDict = dict()
+
+    # Iterate over all (k,v) pairs in names
+    for key, value in d.items():
+        # Is condition satisfied?
+        if f(key, value):
+            newDict[key] = value
+
+    return newDict
 
 bot.run()
-# now invite the bot to ur server,
-# and type '.hello'(in any channel) to check ur san today!
-# (remember to grant read & send permissions to the bot first)
